8d7d8e76f2bf37ea511481af15ddcd780560bf9f
Jorge Manrubia <jorge@37signals.com>
2025-04-07 12:45:53 +0200

    Avoid # since it confuses screen readers

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -8,7 +8,7 @@ module NavigationHelper
   end
 
   def link_to_back(fallback_path: root_path)
-    link_to "#", class: "btn flex-item-justify-start", data: { controller: "back-navigation hotkey", action: "keydown.esc@document->hotkey#click", back_navigation_fallback_destination_value: fallback_path } do
+    link_to fallback_path, class: "btn flex-item-justify-start", data: { controller: "back-navigation hotkey", action: "keydown.esc@document->hotkey#click", back_navigation_fallback_destination_value: fallback_path } do
       icon_tag("arrow-left") + tag.span("Go Back", class: "for-screen-reader")
     end
   end
